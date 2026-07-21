0336c2be78932bda4977747f93f444486da84fdf
rankirpedare <rankir@pedarecc.sa.edu.au>
2026-07-21 03:34:06 +0000

    made starter.py code to decode the raw phrase to a clean phrase

--- starter.py
+++ starter.py
@@ -1,7 +1,10 @@
-# The raw passphrase to clean:
-raw_phrase = "aP!pL3e#S4aU%cE"
+raw_phrase = 'aP!pL3e#S4aU%cE'
+symbols_to_remove = "!#%"
+clean_phrase = ""
 
+for char in raw_phrase:
+    if char not in symbols_to_remove and not char.isdigit():
+        clean_phrase += char
 
-
-# YOUR GOAL: Clean up this phrase using the 3 Security Rules below!
-
+final_code = clean_phrase.upper()
+print(f"Decoded Code: {final_code}")
